23203de450611f9576e95b20047573e30d4568bd
Yike Lu <yikelu.home@gmail.com>
2012-07-28 17:53:25 -0400

    Added switch window and mic paren.

--- init.el
+++ init.el
@@ -192,6 +192,9 @@
 ;; This is your old M-x.
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
+(require 'mic-paren)
+(paren-activate)
+
 (require 'org-install)
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-cc" 'org-capture)
